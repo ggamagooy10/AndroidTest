b470425cc0e9b6e0dce87a8d169ad83b3570e1d7
ggamagooy10 <63840864+ggamagooy10@users.noreply.github.com>
2020-04-29 17:46:16 +0900

    이미지
    ㅜㅜ

--- Project3_2/app/src/main/java/com/example/project3_2/MainActivity.java
+++ Project3_2/app/src/main/java/com/example/project3_2/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.project3_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup1;
+    RadioButton Radgawe, Radbo, Radbawe;
+    Button btnOK;
+    ImageView Imggame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("가위바위보 사진 보기");
+
+        text1 = (TextView) findViewById(R.id.Text1);
+        text2 = (TextView) findViewById(R.id.Text2);
+
+        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
+        Radgawe = (RadioButton) findViewById(R.id.Radgawe);
+        Radbawe = (RadioButton) findViewById(R.id.Radbawe);
+        Radbo = (RadioButton) findViewById(R.id.Radbo);
+        btnOK = (Button) findViewById(R.id.BtnOK);
+        Imggame = (ImageView) findViewById(R.id.Imggame);
+
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(chkAgree.isChecked()==true){
+                    text2.setVisibility(View.VISIBLE);
+                    rGroup1.setVisibility(View.VISIBLE);
+                    btnOK.setVisibility(View.VISIBLE);
+                    Imggame.setVisibility(View.VISIBLE);
+                }else{
+                    text2.setVisibility(View.INVISIBLE);
+                    rGroup1.setVisibility(View.INVISIBLE);
+                    btnOK.setVisibility(View.INVISIBLE);
+                    Imggame.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+
+
     }
 }
